8c370eca53306487e0054332a6aea716f11fbfe7
iDavidLin <linyongxi7@gmail.com>
2021-08-15 13:21:33 +1000

    add hydra

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -175,3 +175,13 @@
   :config
   (evil-collection-init))
 
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(rune/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale text"))
