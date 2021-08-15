1cc95040b0cc516f3be48a67fdc5e9205412c7f5
iDavidLin <linyongxi7@gmail.com>
2021-08-15 15:36:40 +1000

    add projectile

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -185,3 +185,22 @@
 
 (rune/leader-keys
   "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+;; Add Peojectile
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/Projects/Code")
+    (setq projectile-project-search-path '("~/Projects/Code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(rune/leader-keys
+  "p" '(projectile-command-map :which-key "Projectile Command Map"))
