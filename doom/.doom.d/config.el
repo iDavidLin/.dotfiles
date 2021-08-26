3903f6cd3f2a44e6dd68a5c8386b06a56beaa312
david.lin <david.lin@myob.com>
2021-08-26 23:17:46 +1000

    replace iisearch with swiper

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -150,6 +150,25 @@
                           (:discard (:tag ("Chore" "Routine" "Daily")))))))))))
 ;; Super agenda:1 ends here
 
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
 ;; org-roam
 (use-package org-roam
   :ensure t
