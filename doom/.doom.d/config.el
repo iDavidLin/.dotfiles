6195138897b1b41f1a770ae854edac08efd21373
david.lin <david.lin@myob.com>
2021-08-26 23:17:07 +1000

    update font

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -22,7 +22,7 @@
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 ;; (setq doom-font (font-spec :size 18 :weight 'semi-light))
-;; (setq doom-font (font-spec :family "Fira Mono" :size 18)) ;
+(setq doom-font (font-spec :family "Fira Code" :size 16 :weight 'semi-light :width 'normal)) ;
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
