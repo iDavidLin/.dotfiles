0b1c8493f9630a193de6c50cc5ae90b43f3eec10
iDavidLin <linyongxi7@gmail.com>
2022-08-10 00:38:12 +1000

    update blog config

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -221,9 +221,9 @@
 (setq org-publish-project-alist
       '(
         ("org-notes"
-         :base-directory "~/Documents/code/org-blog/org/"
+         :base-directory "~/blog"
          :base-extension "org"
-         :publishing-directory "~/html/"
+         :publishing-directory "~/blog/html/"
          :recursive t
          :publishing-function org-html-publish-to-html
          :headline-levels 4             ; Just the default for this project.
@@ -231,9 +231,9 @@
          :html-preamble "<ul class=\"nav\"><li><a href=\"index.html\">home</a></li><li><a href=\"career.html\">career</a></li><li><a href=\"life.html\">life</a></li></ul>"
          )
         ("org-static"
-         :base-directory "~/Documents/code/org-blog/org/"
+         :base-directory "~/blog"
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-         :publishing-directory "~/html/"
+         :publishing-directory "~/blog/html/"
          :recursive t
          :publishing-function org-publish-attachment
          )
